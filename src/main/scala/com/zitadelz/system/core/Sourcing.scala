73f19ab7db44055bec88c3ba1fafa3eb3081c983
nzf <nd.fluffy@gmail.com>
2014-07-07 07:31:52 -0700

    Default empty Envelope

--- src/main/scala/com/zitadelz/system/core/Sourcing.scala
+++ src/main/scala/com/zitadelz/system/core/Sourcing.scala
@@ -2,16 +2,42 @@ package com.zitadelz.system.core
 
 import language.higherKinds
 
+import com.google.protobuf._
+
 import net.sandrogrzicic.scalabuff.Message
 import net.sandrogrzicic.scalabuff.Parser
 
 import akka.actor.ActorRef
 
 trait Sourcing[Command] {
+
   case class Envelope(payload: Command, sequenceNr: Long, senderStack: List[ActorRef])
+
+  def empty: Envelope = Envelope(dummy, 0L, Nil)
+
+  protected def dummy: Command
 }
 
 object Sourcing extends Sourcing[Message[_] with Parser[_]] {
   type Command = Message[_] with Parser[_]
   type Envelope = super.Envelope
+
+  protected def dummy: Command = new Message[Nothing] with Parser[Nothing] {
+    // Members declared in com.google.protobuf.MessageLite.Builder
+    def build(): MessageLite = ???
+    def buildPartial(): MessageLite = ???
+    def clear(): MessageLite.Builder = ???
+
+    // Members declared in net.sandrogrzicic.scalabuff.MessageBuilder
+    def isInitialized: Boolean = ???
+    def mergeFrom(input: CodedInputStream, extensionRegistry: ExtensionRegistryLite): Nothing = ???
+    def mergeFrom(message: Nothing): Nothing = ???
+    def toByteArray(): Array[Byte] = ???
+
+    // Members declared in com.google.protobuf.MessageLiteOrBuilder
+    def getDefaultInstanceForType(): MessageLite = ???
+
+    // Members declared in com.google.protobuf.Parser
+    def parsePartialFrom(x$1: CodedInputStream, x$2: ExtensionRegistryLite): Nothing = ???
+  }
 }
