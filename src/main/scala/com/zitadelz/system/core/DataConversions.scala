d96d6aee7451e8444933d45bfdbb9ac8df409b68
nzf <nd.fluffy@gmail.com>
2014-09-18 01:34:26 -0700

    Types supporting data conversion to/from JSON and Protocol Buffer

---
+++ src/main/scala/com/zitadelz/system/core/DataConversions.scala
@@ -0,0 +1,17 @@
+package com.zitadelz.system.core
+
+import com.google.protobuf.MessageLite
+import net.sandrogrzicic.scalabuff.Message
+
+object Predef {
+  private[core]type Protuf[P <: Protuf[P]] = MessageLite with MessageLite.Builder with Message[P] {
+    def msb: Long
+    def lsb: Long
+  }
+}
+
+trait ProtufConv[I <: HasId[I], P <: Predef.Protuf[P]] {
+  def protuf(msb: Long, lsb: Long): P
+}
+
+case class WithPrefix[T](prefix: Int) extends AnyVal
