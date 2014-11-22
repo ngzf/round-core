8bf9af22e95bbbd8142600c12b2d4e41b464d055
nzf <nd.fluffy@gmail.com>
2014-11-22 11:14:40 -0800

    Be more generous in Identifiers for testing

--- src/test/scala/com/zitadelz/system/core/TestIdentfier.scala
+++ src/test/scala/com/zitadelz/system/core/TestIdentfier.scala
@@ -1,5 +1,13 @@
 package com.zitadelz.system.core
 
 object TestIdentifier {
-  def apply[A <: HasId[A]]: Identifier[A] = Identifier[A](0, 0)
+  def apply[A <: HasId[A]]: Identifier[A] = TestIdentifier[A](0, 0)
+}
+
+case class TestIdentifier[A <: HasId[A]](msb: Long, lsb: Long) extends Identifier[A] {
+  override def datacenterId: Long = -1
+  override def generatorId: Long = -1
+  override def sequenceNr: Long = lsb
+  override def counter: Long = -1
+  override lazy val base64: String = s"TestIdentifierBase64($msb, $lsb)"
 }
