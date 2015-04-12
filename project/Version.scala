e43e4f1806da281f41703ccbe4902d59ef6d9b06
nzf <nd.fluffy@gmail.com>
2015-04-12 10:46:11 -0700

    Update version of Scala, Akka, Play and SBT

--- project/Version.scala
+++ project/Version.scala
@@ -1,6 +1,6 @@
 object Version {
-  val akkaVersion = "2.3.7"
-  val playVersion = "2.3.6"
-  val scalaVersion = "2.11.4"
+  val akkaVersion = "2.3.9"
+  val playVersion = "2.3.8"
+  val scalaVersion = "2.11.6"
   val zitadelzVersion = "0.1-SNAPSHOT"
 }
