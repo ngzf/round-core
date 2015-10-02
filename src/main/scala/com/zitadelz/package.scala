f336120d2bc63eef03bf7a4f3b006a931a20f21b
nzf <duy@groowin.com>
2015-10-01 23:44:49 -0700

    Reformat code

--- src/main/scala/com/zitadelz/package.scala
+++ src/main/scala/com/zitadelz/package.scala
@@ -6,7 +6,7 @@ import scalaz.syntax.ToValidationOps
 
 package object zitadelz extends ToValidationOps with ToNelOps {
 
-  type ->[+A, +B] = (A, B)
+  type -> [+A, +B] = (A, B)
   object -> { @inline def unapply[A, B](t: A -> B): Option[A -> B] = Tuple2.unapply(t) }
 
   type Validation[+A] = ValidationNel[String, A]
