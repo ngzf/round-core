a29c5eb3919d8042901d0a899ea8cf194f3c09bd
nzf <duy@groowin.com>
2015-10-01 22:50:03 -0700

    Some more frequently used artifact versions

--- project/Version.scala
+++ project/Version.scala
@@ -2,7 +2,12 @@ object Version {
   val akkaVersion = "2.4.0"
   val playVersion = "2.4.3"
   val scalaVersion = "2.11.7"
-  
+
+  val scalazVersion = "7.1.4"
+  val scalabuffVersion = "1.4.0"
+  val specs2Version = "3.6.4"
+  val logbackVersion = "1.1.3"
+
   private def major: Int = 1
   private def minor: Int = 0
   private def fix: Int = 0
