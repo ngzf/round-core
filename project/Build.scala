48d4bebf4469274146eb935578808c06191db7fa
nzf <duy@groowin.com>
2015-11-23 20:02:47 -0800

    Remove Scalariform plugin as we have in Round Build

--- project/Build.scala
+++ project/Build.scala
@@ -3,24 +3,9 @@ import Keys._
 
 import com.round.sbt.SbtRound.autoImport._
 
-import com.typesafe.sbt.SbtScalariform
-import com.typesafe.sbt.SbtScalariform.ScalariformKeys
-
-import scalariform.formatter.preferences._
-
 object ApplicationBuild extends Build {
 
-  lazy val formattingPreferences = FormattingPreferences()
-    .setPreference(AlignParameters, true)
-    .setPreference(AlignSingleLineCaseStatements, true)
-    .setPreference(DoubleIndentClassDeclaration, true)
-    .setPreference(SpacesAroundMultiImports, true) // this was changed in 0.1.7 scalariform, setting this to preserve default.
-
-  lazy val formatSettings = SbtScalariform.scalariformSettings ++ Seq(
-    ScalariformKeys.preferences in Compile := formattingPreferences,
-    ScalariformKeys.preferences in Test := formattingPreferences)
-
-  val buildSettings = Defaults.coreDefaultSettings ++ roundSettings ++ formatSettings
+  val buildSettings = Defaults.coreDefaultSettings ++ roundSettings
 
   lazy val root = Project(id = "root", base = file("."), settings = buildSettings)
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,1 @@
 addSbtPlugin("com.round" %% "round-build" % Version.ours())
-
-addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.5.1")
