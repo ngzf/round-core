5c64bce88d181f728d0be861849e3f7ffae716c8
nzf <ngocduy.pham@outlook.com>
2017-05-28 20:18:33 -0700

    Elasticsearch mapping support for Location and Address

--- dependency.sbt
+++ dependency.sbt
@@ -1,7 +1,7 @@
 libraryDependencies ++= Seq(
   "commons-codec" % "commons-codec" % "1.10",
   "joda-time" % "joda-time" % "2.9.9",
-  Library.scalapb(),
+  Library.elastic4s("core"),
   Library.play(),
   Library.scalaz(),
   Library.scalapb(),
--- src/main/scala/com/round/connector/package.scala
+++ src/main/scala/com/round/connector/package.scala
@@ -2,14 +2,32 @@ package com.round
 
 import play.api.libs.json._
 
+import com.sksamuel.elastic4s.ElasticApi._
+
 package object connector {
 
   trait LocationJson { _: Location.type =>
     implicit val jsonFormat: Format[Location] = Json.format[Location]
+
+    val inMapping = objectField("location").fields(
+      Address.inMapping,
+      doubleField("latitude"),
+      doubleField("longtitude")
+    )
   }
 
   trait AddressJson { _: Address.type =>
     implicit val jsonFormat: Format[Address] = Json.format[Address]
+
+    val inMapping = objectField("address").fields(
+      textField("line1"),
+      textField("line2"),
+      textField("ward"),
+      textField("district"),
+      textField("city"),
+      textField("province"),
+      textField("country")
+    )
   }
 
   implicit val genderFormat: Format[Gender] = new Format[Gender] {
