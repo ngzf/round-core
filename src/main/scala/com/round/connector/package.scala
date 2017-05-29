43b37864230000d992d65210b0f5cc91cd92d9e9
nzf <ngocduy.pham@outlook.com>
2017-05-28 20:04:39 -0700

    Specialize Vietnamese addresses

--- src/main/scala/com/round/connector/package.scala
+++ src/main/scala/com/round/connector/package.scala
@@ -5,7 +5,11 @@ import play.api.libs.json._
 package object connector {
 
   trait LocationJson { _: Location.type =>
-    implicit val jsonFormat = Json.format[Location]
+    implicit val jsonFormat: Format[Location] = Json.format[Location]
+  }
+
+  trait AddressJson { _: Address.type =>
+    implicit val jsonFormat: Format[Address] = Json.format[Address]
   }
 
   implicit val genderFormat: Format[Gender] = new Format[Gender] {
