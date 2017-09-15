5daf15df1638b6dd8397daf65ed4d576ae2ee8e7
duyp <duyp@cakesolutions.net>
2017-09-15 10:11:56 +0100

    Migrate from joda time to Java time for Play 2.6.x

--- dependency.sbt
+++ dependency.sbt
@@ -1,6 +1,5 @@
 libraryDependencies ++= Seq(
   "commons-codec" % "commons-codec" % "1.10",
-  "joda-time" % "joda-time" % "2.9.9",
   Library.elastic4s("core"),
   Library.play(),
   Library.scalaz(),
--- src/main/scala/com/round/TimeMapper.scala
+++ src/main/scala/com/round/TimeMapper.scala
@@ -1,12 +1,11 @@
 package com.round
 
-import org.joda.time._
+import java.time._
 
 import com.trueaccord.scalapb.TypeMapper
 
 trait TimeMapper {
-  implicit val yearMonthTypeMapper = TypeMapper(YearMonth.parse)(_.toString)
-  implicit val dateTimeTypeMapper = TypeMapper[Long, DateTime](new DateTime(_))(_.getMillis)
-  implicit val localDateTypeMapper = TypeMapper(LocalDate.parse)(_.toString)
-  implicit val localTimeTypeMapper = TypeMapper(LocalTime.fromMillisOfDay)(_.getMillisOfDay)
+  implicit val yearMonthTypeMapper = TypeMapper[String, YearMonth](YearMonth.parse)(_.toString)
+  implicit val localDateTypeMapper = TypeMapper[String, LocalDate](LocalDate.parse)(_.toString)
+  implicit val localTimeTypeMapper = TypeMapper(LocalTime.ofNanoOfDay)(_.toNanoOfDay())
 }
